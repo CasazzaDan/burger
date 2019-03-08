8f6a68f83dfb8edd561e5c0d060c8faae1084b52
DCas973 <dan.casazza@endeveran.com>
2019-03-07 22:49:34 -0600

    coding but still working out bugs, getting ready to deploy to heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,16 +4,37 @@ var burger = require("../models/burger");
 
 // Set up our app routes
 
-router.get("/", function(req, res) {
-
+router.get("/", function (req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject);
+    });
 });
 
-router.post("/api/burgers", function(req, res) {
-
+router.post("/api/burgers", function (req, res) {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], 
+    function(result){
+        res.json({id: result.insertId});
+    });
 });
 
-router.put("/api/burgers/:id", function(req, res) {
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
 
+    burger.updateOne(
+        {
+            devour: req.body.devoured
+        },
+        condition,
+        function (result) {
+            if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
 });
 
 module.exports = router;
---
+++ db/seed.sql
@@ -0,0 +1,2 @@
+INSERT INTO burgers (burger_name) VALUES ("Cowboy Burger");
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hangover Burger", true);
